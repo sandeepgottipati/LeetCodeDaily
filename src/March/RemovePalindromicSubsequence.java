1e5d537e729cfa833cd1d82d3b4608fa6b654f32
sandeepgottipati <snchowdary7@gmail.com>
2022-06-08 11:15:30 -0500

    "Commited RemovePalindromicSubsequence"

---
+++ src/March/RemovePalindromicSubsequence.java
@@ -0,0 +1,36 @@
+package March;
+/*problem description:
+link: https://leetcode.com/problems/remove-palindromic-subsequences/
+ */
+//Time complexity:O(n)
+//space complexity:O(1)
+public class RemovePalindromicSubsequence {
+    public int removePalindromicSubsequence(String s){
+        if(s.length()==0 || s=="") return 0;
+        if(isPalindrome(s)){
+            return 1;
+        }
+        else{
+            return 2;
+        }
+    }
+    private boolean isPalindrome(String s){
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        RemovePalindromicSubsequence obj=new RemovePalindromicSubsequence();
+        System.out.println(obj.removePalindromicSubsequence("abbbab"));
+    }
+}
